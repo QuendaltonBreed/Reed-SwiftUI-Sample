2619c5107328b578ca7552f93635b8eb33bc4fb9
Brendan Reed <b.reed@setonhill.edu>
2020-01-28 12:02:01 -0500

    Initial Commit

---
+++ Messner SwiftUI Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messner SwiftUI Sample
+//
+//  Created by Brendan Reed on 1/28/20.
+//  Copyright © 2020 Brendan Reed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
